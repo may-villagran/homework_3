69f36d12ad86fe1aadf879893ca99850bf4a6a78
may-villagran <maylenvillagran2004@gmail.com>
2025-05-15 23:03:15 +0000

    sources de mediciones

---
+++ ejercicio_2/include/Figuras.hpp
@@ -0,0 +1,29 @@
+
+
+
+class Punto{
+    private:
+        int x;
+        int y;
+    public:
+        void set_coordenada_x(int x);
+        void set_coordenada_y(int y);
+        void set_coordenadas(int x, int y);
+
+};
+
+
+class Circulo{
+
+};
+
+
+class Elipse{
+
+};
+
+class Rectangulo{
+
+};
+
+
---
+++ ejercicio_2/include/ProceesadorFigura.hpp
@@ -0,0 +1,8 @@
+#include "Figuras.hpp"
+
+class ProcesadorFigura{
+    public: 
+        void calcular_area();
+        
+
+};
